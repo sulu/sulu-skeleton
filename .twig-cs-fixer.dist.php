e45c42ba0389a7231a1e08e6712d518da5d4823e
Alexander Schranz <alexander@sulu.io>
2024-08-19 10:15:56 +0200

    Add Twig CS Fixer for Skeleton

---
+++ .twig-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+$ruleset = new TwigCsFixer\Ruleset\Ruleset();
+$ruleset->addStandard(new TwigCsFixer\Standard\TwigCsFixer());
+
+$finder = new TwigCsFixer\File\Finder();
+$finder->in('templates/');
+
+$config = new TwigCsFixer\Config\Config();
+$config->allowNonFixableRules();
+$config->setRuleset($ruleset);
+$config->setFinder($finder);
+
+// $config->addTwigExtension(new Sulu\Twig\Extensions\PortalExtension());
+// $config->addTokenParser(new Sulu\Twig\Extensions\TokenParser\PortalTokenParser());
+
+return $config;
